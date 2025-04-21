2fc1ef4ae6be7408526da358336b8f9b57460f19
VictorSouzaSilva <victorsouzaPSCA@gmail.com>
2025-04-21 19:24:01 -0300

    Update tela_cadastro.js

--- frontend/components/cadastro_login/tela_cadastro.js
+++ frontend/components/cadastro_login/tela_cadastro.js
@@ -73,7 +73,7 @@ export default function CriarConta({ navigation }) {
 }
 
 
-const styles = StyleSheet.create({
+const stylesCriarConta = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#1e1a1a',
